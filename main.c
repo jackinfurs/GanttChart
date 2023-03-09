c9eecef68509c5a2ed524b0c71840711ed738566
Martina D'Argenzio <martina.dargenzio@ucdconnect.ie>
2023-03-09 16:45:25 +0000

    redone main.c

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main()
 
     printf ("Welcome to the Gantt Generator\n");
     printf("Would you like to use the test example or create your own Gantt from scratch? (yes or no)\n");
-    scanf("%s", &options);
+    scanf(" %s", &options);
 
 }
