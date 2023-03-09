f77bb09ce84e88657596346c7f9583ab2fac3728
Jack D <jack.dunne4@ucdconnect.ie>
2023-03-09 17:03:14 +0000

    actually begin the project now???

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 
 // https://www.youtube.com/watch?v=USjZcfj8yxE
 
-int main()
-{
+int main() {
     char options;
 
     printf ("Welcome to the Gantt Generator\n");
-    printf("Would you like to use the test example or create your own Gantt from scratch??? (yes or no)\n");
+    printf("Would you like to use the test example or create your own Gantt from scratch? (yes or no)\n");
     scanf(" %s", &options);
 
 }
