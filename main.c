fb23e2575772fc27e9f29ece375520e338cfc758
Jack D <jack.dunne4@ucdconnect.ie>
2023-03-09 16:56:20 +0000

    branch test (DEV)

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
     char options;
 
     printf ("Welcome to the Gantt Generator\n");
-    printf("Would you like to use the test example or create your own Gantt from scratch? (yes or no)\n");
+    printf("Would you like to use the test example or create your own Gantt from scratch??? (yes or no)\n");
     scanf(" %s", &options);
 
 }
