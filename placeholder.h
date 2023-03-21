4ba8ea0c30987dad4603d421f6e84738251b7795
Jack D <jack.dunne4@ucdconnect.ie>
2023-03-21 18:24:00 +0000

    BATTERY MIGHT RUN OUT

--- placeholder.h
+++ placeholder.h
@@ -12,7 +12,21 @@ typedef struct
     int numbOfDepen;
     int dependencies[];
 } Task;
-
+Task placeholder[MAX_TASKS] = {{
+                                .name = "task_1",
+                                .start_month = 1,
+                                .end_month = 2,
+                                .numbOfDepen = 0,
+                                .dependencies = {0}},
+                               {"task_2",1,4,0,{0}},
+                               {"task_3",3,5,1,{0}},
+        {"task_4",2,4,2,{0, 1}},
+        {"task_5",4,5,1,{2}},
+        {"task_6",6,7,2,{3, 4}},
+        {"task_7",7,7,1,{4}},
+        {"task_8",8,9,1,{6}},
+        {"task_9",8,10,1,{4}},
+        {"task_10",11,12,2,{7,8}}};
 Task task1 = {
         .name = "task_1",
         .start_month = 1,
