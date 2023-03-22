073392e76df4aca6517679b0dfa28654c8acb3c4
Jack D <jack.dunne4@ucdconnect.ie>
2023-03-22 13:48:03 +0000

    whoops

--- struct.h
+++ struct.h
@@ -1,8 +1,12 @@
 #ifndef BO_ASSIGNMENT_2_STRUCT_H
 #define BO_ASSIGNMENT_2_STRUCT_H
 
-#include "struct.c"
-
-struct Task;
+struct Task {
+    char name[MAX_LENGTH];
+    int start_month;
+    int end_month;
+    int numbOfDepen;
+    int dependencies[MAX_TASKS];
+};
 
 #endif //BO_ASSIGNMENT_2_STRUCT_H
